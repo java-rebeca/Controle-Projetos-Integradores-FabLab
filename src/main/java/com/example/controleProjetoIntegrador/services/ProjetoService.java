0d48cf914248b9036bd1e8d639a5f5c58df651f6
Gi <geovanna.telles@aluno.senai.br>
2025-11-19 11:37:32 -0300

    Alterando lógica de FK do projeto

--- src/main/java/com/example/controleProjetoIntegrador/services/ProjetoService.java
+++ src/main/java/com/example/controleProjetoIntegrador/services/ProjetoService.java
@@ -3,7 +3,11 @@ package com.example.controleProjetoIntegrador.services;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.example.controleProjetoIntegrador.repositories.ProjetoRepository;
+import com.example.controleProjetoIntegrador.repositories.AgendaRepository;
+import com.example.controleProjetoIntegrador.repositories.UsuarioRepository;
 import com.example.controleProjetoIntegrador.models.Projeto;
+import com.example.controleProjetoIntegrador.models.Agenda;
+import com.example.controleProjetoIntegrador.models.Usuario;
 import java.util.List;
 
 
@@ -12,6 +16,8 @@ public class ProjetoService {
 
     @Autowired
     private ProjetoRepository pr;
+    private UsuarioRepository ur;
+    private AgendaRepository ar;
 
     public Projeto cadastrarProjeto(Projeto p){
         return pr.save(p);
@@ -29,17 +35,31 @@ public class ProjetoService {
         return pr.findAll();
     }
 
-    public Projeto atualizarProjeto(Projeto p, Integer id){
+    public Projeto atualizarProjeto(Projeto p, Integer id, Integer idUsuario, Integer idAgenda){
         Projeto _p = pr.findById(id).orElse(null);
         if (_p == null) {
             return null;
         }
-        _p.setNome(p.getNome());
-        _p.setDescricao(p.getDescricao());
-        _p.setCurso(p.getCurso());
-        _p.setAlunos(p.getAlunos());
-        _p.setUsuario(p.getUsuario());
-        _p.setAgenda(p.getAgenda());
+        if (p.getNome() != null){
+            _p.setNome(p.getNome());
+        }
+        if (p.getDescricao() != null){
+            _p.setDescricao(p.getDescricao());
+        }
+        if (p.getCurso() != null){
+            _p.setCurso(p.getCurso());
+        }
+        if (p.getAlunos() != null){
+            _p.setAlunos(p.getAlunos());
+        }
+        if (idUsuario != null){
+            Usuario usr = ur.findById(idUsuario).orElse(null);
+            _p.setUsuario(usr);
+        }
+        if (idAgenda != null){
+            Agenda ag = ar.findById(idAgenda).orElse(null);
+            _p.setAgenda(ag);
+        }
         return pr.save(_p);
     }
 }
